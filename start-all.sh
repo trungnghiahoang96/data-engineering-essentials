237495e4009207731c3bd7866c208e746e786ac0
Trung Nghĩa Hoàng <46636991+trungnghiahoang96@users.noreply.github.com>
2021-08-24 18:04:32 +0700

    set up Hadoop, Spark with sdk man, Hive on local

---
+++ start-all.sh
@@ -0,0 +1,5 @@
+$HADOOP_HOME/sbin/start-dfs.sh 
+$HADOOP_HOME/sbin/start-yarn.sh
+docker start cluster_util_db
+jps
+docker ps 
---
+++ stop-all.sh
@@ -0,0 +1,3 @@
+$HADOOP_HOME/sbin/stop-yarn.sh
+$HADOOP_HOME/sbin/stop-dfs.sh
+jps
